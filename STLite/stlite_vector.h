bafc0e157cf02b74d68ec4dde9f756a357c23243
githubdlj <511325085@qq.com>
2015-11-01 20:07:40 +0800

    修复insert函数bug

--- STLite/stlite_vector.h
+++ STLite/stlite_vector.h
@@ -21,7 +21,8 @@ namespace STLite
     //////////////////////////////////////////////////////////////////////
     //  vectorIterator
     template<class T>
-    class vectorIterator : public iterator<random_access_iterator<T, std::ptrdiff_t>, T>
+//    class vectorIterator : public iterator<random_access_iterator<T, std::ptrdiff_t>, T>
+    class vectorIterator : public iterator<random_access_iterator_tag, std::ptrdiff_t>
     {
     private:
         T *m_ptr;
@@ -197,15 +198,42 @@ namespace STLite
             finish = start + n;
             end_of_storage = finish;
         }
-        
+ 
+//         //   SGI STL源码中，针对InputIterator和ForwardIterator有单独的实现       
+//         template<class InputIterator>
+//         void allocate_and_copy(InputIterator first, InputIterator last)
+//         {
+//              start = allocate(last - first);     //  重载迭代器减法
+//              finish = uninitialized_copy(first, last, start);
+//              end_of_storage = finish;
+//         }
+
         template<class InputIterator>
-        void allocate_and_copy(InputIterator first, InputIterator last)
+        void allocate_and_copy(InputIterator first, InputIterator last, input_iterator_tag)
         {
-             start = allocate(last - first);     //  重载迭代器减法
-             finish = uninitialized_copy(first, last, start);
-             end_of_storage = finish;
+            //  do something, different from ForwardIterator
+            //  because Input iterators are iterators that can be used in sequential input operations, 
+            //  where each value pointed by the iterator is read only once and then the iterator is incremented.
+            
+            //  like this
+//             for (; first != last; first++)
+//             {
+//                 push_back(*first); 
+//             }
+
         }
-        
+
+        template<class ForwardIterator>
+        void allocate_and_copy(ForwardIterator first, ForwardIterator last, forward_iterator_tag)
+        {
+            size_type n = (size_type) distance(first, last);
+            
+            start = allocate(n);
+            finish = uninitialized_copy(first, last, start);
+            end_of_storage = start + n;
+        }
+
+        //////////////////////////////////////////////////////////////////////
         void destroy_and_deallocate()
         {
             if (start != end_of_storage)
@@ -225,7 +253,8 @@ namespace STLite
         template<class InputIterator>
         void vector_aux(InputIterator first, InputIterator last, __false_type)
         {
-            allocate_and_copy(first, last);
+            typedef typename iterator_traits<InputIterator>::iterator_category category;
+            allocate_and_copy(first, last, category());
         }   
 
     //////////////////////////////////////////////////////////////////////
@@ -250,7 +279,10 @@ namespace STLite
         //  copy
         vector(const vector &lhs)
         {
-            allocate_and_copy(lhs.start, lhs.finish);
+        //    allocate_and_copy(lhs.start, lhs.finish, iterator_category(iter));
+            start = allocate(lhs.finish - lhs.start);
+            finish = uninitialized_copy(lhs.start, lhs.finish, start);
+            end_of_storage = finish;
         }
     
         //  assignment, 注意，赋值的时候要释放原来所指向的内存空间
@@ -363,9 +395,9 @@ namespace STLite
         }
     //////////////////////////////////////////////////////////////////////
     //  Modifiers
-    //  insert_aux
+    //  insert 
     private:
-        void insert_aux(iterator pos, size_type n, const T &value, __true_type)
+        void fill_insert(iterator pos, size_type n, const T &value)
         {
             size_type locationLeft = end_of_storage - finish;
             size_type locationNeed = n;
@@ -405,13 +437,27 @@ namespace STLite
                 end_of_storage = new_end_of_storage;
             }
         }
-
+        //////////////////////////////////////////////////////////////////////
+        //  the SGI STL has different range_insert version for InputIterator & ForwardIterator
         template<class InputIterator>
-        void insert_aux(iterator pos, InputIterator first, InputIterator last, __false_type)
+        void range_insert(iterator pos, InputIterator first, InputIterator last, input_iterator_tag)
+        {
+            //  can not calculate the capacity that the new elements need, it only can be insertrd one by one 
+
+            //  insert one by one, like this
+//             for (; first != last; first++)
+//             {
+//                 pos = insert(pos, *first);
+//                 pos++
+//             }
+        }
+
+        template<class ForwardIterator>
+        void range_insert(iterator pos, ForwardIterator first, ForwardIterator last, forward_iterator_tag)
         {
             size_type locationLeft  = end_of_storage - finish;
-            size_type locationNeed = last - first;
-
+            size_type locationNeed = (size_type) distance(first, last);
+           
             if (locationNeed <= locationLeft)
             {
                 iterator temp = pos;
@@ -420,7 +466,7 @@ namespace STLite
                     *(temp + locationNeed) = *(temp);
                     temp++;
                 }
-                uninitialized_copy(first, last, pos);
+                STLite::uninitialized_copy(first, last, pos);
                 finish = finish + locationNeed;
             }
             else
@@ -433,9 +479,9 @@ namespace STLite
                 pointer new_end_of_storage = new_start + new_capacity;
                 
                 //  copy
-                pointer new_finish = uninitialized_copy(begin(), pos, new_start);
-                new_finish = uninitialized_copy(first, last, new_finish);
-                new_finish = uninitialized_copy(pos, end(), new_finish);
+                pointer new_finish = STLite::uninitialized_copy(begin(), pos, new_start);
+                new_finish = STLite::uninitialized_copy(first, last, new_finish);
+                new_finish = STLite::uninitialized_copy(pos, end(), new_finish);
 
                 //  free old space
                 destroy_and_deallocate();
@@ -445,20 +491,34 @@ namespace STLite
                 finish = new_finish;
                 end_of_storage = new_end_of_storage;
             }
-              
         }
+        //////////////////////////////////////////////////////////////////////
+        template<class InputIterator> 
+        void insert_dispatch(iterator pos, InputIterator first, InputIterator last, __false_type)
+        {
+            typedef typename iterator_traits<InputIterator>::iterator_category category;
+            range_insert(pos, first, last, category());
+        }
+    
+        template<class Integer>
+        void insert_dispatch(iterator pos, Integer n, const value_type &x, __true_type)
+        {
+            fill_insert(pos, n, x);
+        }
+         
     //  insert
     public:
+      
         void insert(iterator pos, size_type n, const T &value)
         {
-            insert_aux(pos, n, value, __true_type());
+            insert_dispatch(pos, n, value, __true_type());
         }
 
         template<class InputIterator>
         void insert(iterator pos, InputIterator first, InputIterator last)
         {
             typedef typename _is_integer<InputIterator>::is_integer is_integer;
-            insert_aux(pos, first, last, is_integer());
+            insert_dispatch(pos, first, last, is_integer());
         }
 
         iterator insert(iterator pos, const T &value)
@@ -474,7 +534,7 @@ namespace STLite
         {   
             //  copy and destroy
             iterator i = uninitialized_copy(last, end(), first);   
-            destroy(i, end());
+            destroy(i, end());              //  erase only destroy elements, not destroy the space!!
     
             finish = finish - (last - first);
             
@@ -484,6 +544,13 @@ namespace STLite
         iterator erase(iterator pos)
         {
             return erase(pos, pos + 1);    //  call erase(first, last)
+        }
+    //  erase
+    public:
+        //  clear only destroy elements, not destroy space !
+        void clear()
+        {
+            erase(begin(), end());
         }
     //////////////////////////////////////////////////////////////////////
     //  push_back, pop_back
@@ -497,8 +564,6 @@ namespace STLite
         {
             erase(end() - 1);
         }
-    };
-
 }
 
 #endif
