5d2e4916d3e86b148902a5bd334b83e4703a2945
githubdlj <511325085@qq.com>
2015-11-25 22:29:39 +0800

    完成仿函数greater，less

---
+++ STLite/stlite_function.h
@@ -0,0 +1,43 @@
+/********************************************************************
+	Time:	  2015/11/25
+	Filename: stlite_function
+	Author:	  dinglj
+	
+	Purpose:  functional
+*********************************************************************/
+
+#ifndef _STLITE_FUNCTION_H_
+#define _STLITE_FUNCTION_H_
+
+//////////////////////////////////////////////////////////////////////
+namespace STLite
+{
+    //  binary function
+    template<class Arg1, class Arg2, class Result>
+    struct binary_function
+    {
+        typedef Arg1 arg1;
+        typedef Arg2 arg2;
+        typedef Result result;
+    };
+
+    template<class T>
+    struct less : public binary_function<T, T, bool>
+    {
+        result operator ()(const arg1 &x, const arg2 &y)
+        {
+            return x < y;
+        }
+    };
+
+    template<class T>
+    struct greater : binary_function<T, T, bool>
+    {
+        result operator ()(const arg1 &x, const arg2 &y)
+        {
+            return x > y;
+        }
+    };
+}
+
+#endif
